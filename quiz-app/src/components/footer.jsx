d58cb14d43b1748360c0aadae5d2ba6a27816905
Yosef <admaseyohanes@gmail.com>
2024-10-08 20:26:16 +0300

    links on footer added

--- quiz-app/src/components/footer.jsx
+++ quiz-app/src/components/footer.jsx
@@ -8,13 +8,13 @@ const Footer = () =>{
             <h2 className="text-lg md:text-xl font-semibold">SafeTech</h2>
           </div>
           <div className="footer-right flex space-x-4">
-            <a href="#" className="text-white ">
+            <a href="https://facebook.com/yoseffekade.yoseffekade" target="blank" className="text-white ">
               <i className="fab fa-facebook"></i>
             </a>
-            <a href="#" className="text-white ">
+            <a href="https://x.com/YoseFFekade?t=BWS83mICJln1zQA9ixQIzg&s=09" target="blank" className="text-white ">
               <i className="fab fa-twitter"></i>
             </a>
-            <a href="#" className="text-white ">
+            <a href="https://instagram.com/yoseffekade" target="blank" className="text-white ">
               <i className="fab fa-instagram"></i>
             </a>
           </div>
