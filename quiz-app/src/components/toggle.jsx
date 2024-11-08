036ed81de1667d361cf81f5f8c831bacd8796d2c
Yosef <admaseyohanes@gmail.com>
2024-11-08 10:52:06 +0300

    toggle css created

--- quiz-app/src/components/toggle.jsx
+++ quiz-app/src/components/toggle.jsx
@@ -13,42 +13,14 @@ export default function DarkModeToggle( {toggleTheme }) {
 
 
   return (
-    <button
-      onClick={toggleTheme }
-      className="p-2 rounded-full bg-gray-200 dark:bg-gray-800 text-gray-900 dark:text-gray-100 transition-colors"
-      aria-label="Toggle dark mode"
-    >
-      {true ? (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            fillRule="evenodd"
-            d="M17.293 17.293a8 8 0 01-11.313-11.313 8 8 0 0011.313 11.313z"
-            clipRule="evenodd"
-          />
-        </svg>
-      ) : (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-6 w-6"
-          fill="currentColor"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-           <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M12 3v1m0 16v1m8-8h1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707M16.95 7.05l.707-.707M7.05 16.95l.707-.707M12 5a7 7 0 100 14 7 7 0 000-14z"
-          />
-          
-        </svg>
-      )}
-    </button>
+    <div>
+      <input  
+      type="checkbox"
+      id='check'
+      className='toggle'
+      onClick={toggleTheme}
+       />
+       <label htmlFor="check"> Dark Mood</label>
+    </div>
   );
 }
