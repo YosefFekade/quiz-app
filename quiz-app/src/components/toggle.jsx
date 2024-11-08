6790d2cfd8bae96b7264e3efe8bac4778a29902f
Yosef <admaseyohanes@gmail.com>
2024-11-08 11:05:21 +0300

    css

--- quiz-app/src/components/toggle.jsx
+++ quiz-app/src/components/toggle.jsx
@@ -1,13 +1,6 @@
-{/* <button
-              onClick={toggleTheme}
-              className="bg-gray-800 text-white py-2 px-4 rounded-md hover:bg-gray-700 dark:bg-gray-200 dark:text-gray-800 dark:hover:bg-gray-300 transition-colors"
-            >
-              {theme === 'light' ? 'Dark Mode' : 'Light Mode'}
-            </button> */}
-
-
-// DarkModeToggle.jsx
 import React from 'react';
+
+import './Toggle.css'
 
 export default function DarkModeToggle( {toggleTheme }) {
 
